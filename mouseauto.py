71d13876ebf449c1758797e84719912b37fa5643
Luis Montenegro <luismontenegro.1995@gmail.com>
2023-03-27 12:07:24 -0300

    Add .py file

---
+++ mouseauto.py
@@ -0,0 +1,52 @@
+import pyautogui
+import random
+import time
+import sys
+import keyboard
+#import ctypes
+
+### Define las constantes necesarias para utilizar la función SetThreadExecutionState
+##ES_CONTINUOUS = 0x80000000
+##ES_SYSTEM_REQUIRED = 0x00000001
+##ES_AWAYMODE_REQUIRED = 0x00000040
+
+#def on_key_press(event):
+#	#print(f"{event.name} was pressed")
+#	if(event.name == 'q'):
+#		print(f"{event.name} was pressed")
+
+# Registrar el evento OnPress
+#keyboard.on_press(on_key_press)
+
+### Indica al sistema operativo que no debe suspender la pantalla
+##ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED)
+
+print("Programa Iniciado. Presione \"Q\" para salir")
+
+
+# Definimos los límites del área en la que queremos mover el mouse
+# en este caso, lo limitamos a la mitad de la pantalla
+x_min, y_min, x_max, y_max = pyautogui.size()[0]//4, pyautogui.size()[1]//4, pyautogui.size()[0]//2, pyautogui.size()[1]//2
+
+while True:
+	# Generamos coordenadas aleatorias dentro del área definida
+	x = random.randint(x_min, x_max)
+	y = random.randint(y_min, y_max)
+
+	# Movemos el mouse a las coordenadas aleatorias
+	pyautogui.moveTo(x, y, duration=0.1)
+
+	# Enviamos una pulsacion a una tecla cualquiera. En este caso a Bloq Num (para evitar que se suspenda el equipo)
+	pyautogui.press('numlock')
+	# Enviamos una segunda pulsacion para volver al estado original del NumLock
+	pyautogui.press('numlock')
+
+	# Esperamos un tiempo aleatorio entre 10 a 60 segundos antes de mover el mouse nuevamente
+	time.sleep(random.randint(10, 60))
+
+# Reactivamos la suspensión de la pantalla cuando el script termina
+# Indica al sistema operativo que se puede suspender la pantalla nuevamente
+###ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
+
+print("Programa terminado.")
+
